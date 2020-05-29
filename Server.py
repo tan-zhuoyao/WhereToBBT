fe761afb05d6fea9e48fdf073ca9a3a49adf605d
tanzhuoyao-nus <65391538+tanzhuoyao-nus@users.noreply.github.com>
2020-05-29 21:47:25 +0800

    Can take in coordinates or text

--- Server.py
+++ Server.py
@@ -18,24 +18,29 @@ while True:
     if updates:
         for item in updates:
             update_id = item["update_id"]
+            message = None
+            longi = None
+            lati = None
             try:
                 #extract out the text sent to bot
                 message = item["message"]["text"]
-                print(message)
             except:
-                #if error then just return default text
-                message = "I did not get you there. Sorry."
+                pass
+
             try:
                 #extract out longitude and latitude
                 longi = item["message"]["location"]["longitude"]
                 lati = item["message"]["location"]["latitude"]
             except:
-                longi = 0
-                lati = 0
-            
+                pass
+
             from_ = item["message"]["from"]["id"]
-            reply = make_reply(message)
-            #bot.send_message(reply, from_)
-            bot.send_message("You longitude is " + str(longi), from_)
-            bot.send_message("Your latitude is " + str(lati), from_)
+            
+            if message is not None:
+                reply = make_reply(message)
+                bot.send_message(reply, from_)
+            else:
+                bot.send_message("You longitude is " + str(longi), from_)
+                bot.send_message("Your latitude is " + str(lati), from_)
+
     
